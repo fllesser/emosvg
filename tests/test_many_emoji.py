0d0b75ab3c0e36ee40c16d31c6a256067d06e398
fllesser <fllessive@gmail.com>
2025-12-10 17:05:51 +0800

    tweak

--- tests/test_many_emoji.py
+++ tests/test_many_emoji.py
@@ -1,3 +1,6 @@
+import time
+import logging
+
 emoji_texts = """
 表情
 😀😁😂😃😄😅😆😉😊😋😎😍😘😗😙😚😇😐😑😶😏😣😥😮😯😪😫😴😌😛😜😝😒😓😔😕😲😷😖😞😟😤😢😭😦😧😨😬😰😱😳😵😡😠
@@ -65,6 +68,7 @@ def test_many_emoji(font_path, cache_dir):
 
     font = ImageFont.truetype(font_path, 24)
     with Image.new("RGB", (3000, 2100), (255, 248, 220)) as image:  # 纸黄背景
+        start = time.time()
         text(
             image,
             (10, 10),
@@ -72,4 +76,28 @@ def test_many_emoji(font_path, cache_dir):
             font,
             fill=(0, 0, 0),
         )
+        end = time.time()
+        logging.info(f"[svg] Cont: {end - start:.2f} seconds")
         image.save(cache_dir / "many_emoji.png")
+
+
+# import pytest
+
+
+# @pytest.mark.asyncio
+# async def test_many_emoji_with_apilmoji(font_path, cache_dir):
+#     from PIL import Image, ImageFont
+#     from apilmoji import Apilmoji, EmojiStyle, EmojiCDNSource
+
+#     font = ImageFont.truetype(font_path, 24)
+#     source = EmojiCDNSource(
+#         cache_dir=cache_dir, style=EmojiStyle.FACEBOOK, enable_tqdm=True
+#     )
+#     with Image.new("RGB", (3000, 2100), (255, 248, 220)) as image:  # 纸黄背景
+#         start = time.time()
+#         await Apilmoji.text(
+#             image, (10, 10), emoji_texts, font, fill=(0, 0, 0), source=source
+#         )
+#         end = time.time()
+#         logging.info(f"[apilmoji] Cont: {end - start:.2f} seconds")
+#         image.save(cache_dir / "many_emoji_async.png")
