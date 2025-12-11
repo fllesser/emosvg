69b38830bf1ee6966a44d42829c747f46ba8b002
fllesser <fllessive@gmail.com>
2025-12-11 12:38:27 +0800

    export import

--- src/emosvg/__init__.py
+++ src/emosvg/__init__.py
@@ -1,3 +1,19 @@
-from .core import text, wrap_text, get_font_size, get_font_height, text_without_wrap
+from .core import (
+    Node,
+    NodeType,
+    text,
+    wrap_text,
+    get_font_size,
+    get_font_height,
+    text_without_wrap,
+)
 
-__all__ = ["get_font_height", "get_font_size", "text", "text_without_wrap", "wrap_text"]
+__all__ = [
+    "Node",
+    "NodeType",
+    "get_font_height",
+    "get_font_size",
+    "text",
+    "text_without_wrap",
+    "wrap_text",
+]
